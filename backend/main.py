030cad27c84d1fbc51a0f39942964bd3852eb881
ErgunVehbi <e_vehbi@outlook.com>
2025-04-29 23:20:38 +0100

    Successfully scraped the elements

--- backend/main.py
+++ backend/main.py
@@ -2,22 +2,45 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+from urllib.request import urlopen
+
+#Prints Title
 
 
-page_to_scrape = requests.get("https://angliacarauctions.co.uk/auctions/2698-23-Apr-2025")
-soup = BeautifulSoup(page_to_scrape.text,"html.parser")
-vehInfos = soup.findAll("div", attrs={"class":"text-secondary card-title fw-bold"})
-vehDescriptions = soup.findAll("div", attrs={"class":"row"})
 
-#file = open("/Users/ergunvehbi/Documents/02_Projects/Python/CarAuction/carList 08-01-25.csv", "w")
-#writer = csv.writer(file)
 
-#writer.writerow(["Vehicle Information", "Vehicle Description"])
 
-for vehInfo, vehDescription in zip(vehInfos, vehDescriptions):
-    if (vehInfo and vehDescription !=""):
-        print(vehInfo.text + " -- "+ vehDescription.text)
-    else:
-        print("Error")
-    #writer.writerow([vehInfo.text, vehDescription.text])
-#file.close()
+
+
+def main():
+    url = "https://angliacarauctions.co.uk/auctions/2700-30-Apr-2025"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    soup.find_all("div")
+    output = soup.select('div.col-xl-4.col-md-6.col-12') 
+    title = soup.select('h3.text-secondary.card-title.fw-bold')
+    car_info = soup.select('table.vehListProp tr')
+
+    list(title)
+    list(car_info)
+    print(output)
+
+    for titles in title:
+        clean_title = titles.text.strip()
+        #print(clean_title)
+        
+    for carinfo in car_info:
+        clean_car_info = carinfo.text.strip()
+        #print(clean_car_info)
+        
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
